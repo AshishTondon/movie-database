9f54a6b9aefd875676e13274c4541c4ec9def32e
Ashish <tondon.ashish@hotmail.com>
2024-12-30 21:40:16 +0530

    added dynamic component

--- src/app/api-doc/react-swagger.tsx
+++ src/app/api-doc/react-swagger.tsx
@@ -1,7 +1,11 @@
-import SwaggerUI from "swagger-ui-react";
+"use client";
+
+import dynamic from "next/dynamic";
 import "swagger-ui-react/swagger-ui.css";
 import "../swagger-custom.css";
 import { IObject } from "@/app/common/api-request";
+
+const SwaggerUI = dynamic(() => import("swagger-ui-react"), { ssr: false });
 
 type IProps = {
   spec: IObject<string | number | object>;
